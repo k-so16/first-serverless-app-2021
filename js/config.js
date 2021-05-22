5fabab747a453e21d85d90a2022074f81d029016
Soichiro KATO <kato-soichiro@msen.jp>
2021-05-22 16:18:05 +0900

    fixed user pool client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_B5oI3wygA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'WildRydesWebApp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2r2nbvbqduoq4lb7rt46aqjddp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
